96edbf54680960d0d7cff669c34783b61baaecf7
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-03-03 14:48:31 -0500

    created database schema and seeded database with mock data

--- database/migrations/20200303122519_add_tables.js
+++ database/migrations/20200303122519_add_tables.js
@@ -3,15 +3,16 @@ exports.up = function(knex) {
     .createTable("users", tbl => {
       tbl.increments();
 
-      tbl.timestamp("created_at").defaultTo(knex.fn.now());
-
       tbl
         .string("username", 128)
         .notNullable()
         .unique();
 
       tbl.string("password", 128).notNullable();
+
+      tbl.timestamp("created_at").defaultTo(knex.fn.now());
     })
+
     .createTable("tasks", tbl => {
       tbl.increments();
 
@@ -24,24 +25,14 @@ exports.up = function(knex) {
         .onUpdate("CASCADE")
         .onDelete("CASCADE");
 
-      tbl.string("task-name", 128).notNullable();
-
-      tbl
-        .timestamp("created_at")
-        .defaultTo(knex.fn.now())
-        .notNullable();
-
-      tbl.dateTime("start").notNullable();
-      tbl.dateTime("end").notNullable();
-      tbl
-        .bool("completed")
-        .notNullable()
-        .defaultTo(false);
+      tbl.string("task_name", 128).notNullable();
+      tbl.dateTime('start').notNullable()
+      tbl.dateTime('end').notNullable()
+      tbl.boolean('completed').notNullable().defaultTo(false)
+      tbl.timestamp('created_at').defaultTo(knex.fn.now())
     });
 };
 
 exports.down = function(knex) {
-  return knex.schema
-  .dropTableIfExists("users")
-  .dropTableIfExists("tasks");
+  return knex.schema.dropTableIfExists("users").dropTableIfExists("tasks");
 };
