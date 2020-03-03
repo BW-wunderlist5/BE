fbd1dbce4cdf010f0243a15d208c7cc151238075
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-03-03 16:50:55 -0500

    set up validation for user

--- users/user-router.js
+++ users/user-router.js
@@ -1,16 +1,21 @@
 const router = require('express').Router();
-
 const Users = require('./user-model.js');
 
+// User can log in and can create, review, update, and delete data on their own todo list.
+
+// Get all users.
 router.get('/', (req, res) => {
   Users.find()
     .then(users => {
       res.status(200).json(users);
     })
-    .catch(err => res.send(err));
+    .catch(err => {
+      res.status(500).json('Problems getting users.', err);
+   })
 });
 
-router.get('/:id', (req, res) => {
+// Get user by id.
+router.get('/:id', validateUser, (req, res) => {
   const id = req.params.id;
   if (!id) {
     res.status(404).json({ message: "The user with the specified id does not exist." });
@@ -25,7 +30,8 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+// Deletes user account
+router.delete('/:id', validateUser, (req, res) => {
   const id = req.params.id;
   if (!id) {
     res.status(404).json({ message: "The user with the specified ID does not exist." })
@@ -39,4 +45,29 @@ router.delete('/:id', (req, res) => {
     })
 });
 
+// Custom middleware to validate user id.
+function validateUser(req, res, next) {
+  const id = req.params.id;
+  let errors = [];
+
+  Users.findById(id)
+     .then(user => {
+        if (!user) {
+           res.status(404).json({ message: 'There is no such user by that id' });
+        } 
+        if (!user.username || user.username.length < 2) {
+          errors.push('Username must contain at least 2 characters');
+        }
+        if (!user.password || user.password.length < 4) {
+          errors.push('Password must contain at least 4 characters');
+        }
+        else {
+          return {
+            isSuccessful: errors.length > 0 ? false : true,
+            errors
+          },
+           next();
+        }
+     });
+}
 module.exports = router;
